a1ae6f7963380f75aadecc2d86125f7239f8ee40
MarcelZDEV <76267990+MarcelZDEV@users.noreply.github.com>
2021-11-26 12:56:21 +0100

    -add new options -add some visual details -update "-rpass"  *add special char

--- main.py
+++ main.py
@@ -4,44 +4,57 @@ import string
 import sys
 
 options = {
-    "-h" : "is showing every options",
-    "-rpass" : "is creating random password",
-    "-end" : "is ending proses"
+    "-h": "is showing every options",
+    "-rpass": "is creating random password",
+    "-restp": "you can change password",
+    "-deluser": "is removing your account",
+    "-end": "is ending proses"
 }
+
 print("Welcome in program")
 
-register = input("Do you have account(Y/N): ")
+print("Do you have account(Y/N): ")
+register = input(">>>")
 
 if register == "N":
-    username = input("Type your name: ")
-    password = input("Type your password: ")
-    phone_number = input("Type your phone number: ")
-        
+    print("Type your name: ")
+    username = input(">>>")
+    print("Type your password: ")
+    password = input(">>>")
+    print("Type your phone number: ")
+    phone_number = input(">>>")
+
     information = {
         "username": username,
         "password": password,
         "phone_number": phone_number
     }
 
-    json_object = json.dumps(information, indent=4)
+    len_username = len(username)
+    len_password = len(password)
+    len_phone = len(phone_number)
 
-    print(json_object)
+    if len_username == 0:
+        print("Wrong user name")
+        sys.exit(1)
+    json_object = json.dumps(information, indent=4)
 
     with open("UserPass.json", "w") as outfile:
         outfile.write(json_object)
-
 elif register == "Y":
     with open('UserPass.json', 'r') as openfile:
         json_object = json.load(openfile)
 
-        username = input("Type your name: ")
-        password = input("Type your password: ")
+        print("Type your name: ")
+        username = input(">>>")
+        print("Type your password: ")
+        password = input(">>>")
         if username and password == json_object["username" and "password"]:
             print("Ok everything went good")
             print("")
             for x, y in options.items():
                 print(x, y)
-            for z in range(random.randint(1000, 1000)):
+            while True:
                 options_quest = input(">>>")
 
                 if options_quest == "-h":
@@ -50,38 +63,83 @@ elif register == "Y":
                 if options_quest == "-rpass":
 
                     num = ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9")
+                    special_let = ("{", "}", "[", "]", "(", ")", "'", "#", "|", "*", "<", ">")
 
 
                     def random_pass():
-                        ran = ''.join(random.choice(string.ascii_letters) for x in range(5))
-                        ran += ''.join(random.choice(num) for x in range(5))
+                        print("How many letters you want: ")
+                        ask_len_pass_let = input(">>>")
+                        print("How many numbers you want: ")
+                        ask_len_pass_num = input(">>>")
+                        print("How many special characters you want: ")
+                        ask_len_pass_spec = input(">>>")
 
-                        to_string = list(ran)
+                        try:
+                            ran = ''.join(random.choice(string.ascii_letters) for x in range(int(ask_len_pass_let)))
+                            ran += ''.join(random.choice(num) for x in range(int(ask_len_pass_num)))
+                            ran += ''.join(random.choice(special_let) for x in range(int(ask_len_pass_spec)))
+                            to_string = list(ran)
 
-                        random.shuffle(to_string)
+                            random.shuffle(to_string)
 
-                        final_pass = ''.join(to_string)
+                            final_pass = ''.join(to_string)
 
-                        return final_pass
+                            return final_pass
+                        except:
+                            print("You must write number")
+                            sys.exit(1)
 
 
                     print("Your password is: ", random_pass())
+                if options_quest == "-deluser":
+                    print("In process of working")
+
+                if options_quest == "-restp":
+                    print("Are you sure you want change password(Y/N)")
+                    change_password_new = input(">>>")
+                    if change_password_new == "Y":
+                        print("Type your name: ")
+                        username = input(">>>")
+                        print("Type your phone number: ")
+                        phone_number = input(">>>")
+
+                        information = {
+                            "username": username,
+                            "password": password,
+                            "phone_number": phone_number
+                        }
+
+                        if username and phone_number == json_object["username" and "phone_number"]:
+                            print("Type new password: ")
+                            change_password_new = input(">>>")
+                            json_object["password"] = change_password_new
+                            password = change_password_new
+
+                            outfile = open("UserPass.json", "w")
+                            json.dump(json_object, outfile)
+                            json_object = json.dumps(information, indent=3)
+                            outfile.close()
+
                 if options_quest == "-end":
                     sys.exit(1)
 
         else:
             print("Something gone wrong")
-            change_password = input("If you don't remember password you can change it(Y/N)")
+            print("If you don't remember password you can change it(Y/N)")
+            change_password = input(">>>")
             if change_password == "Y":
-                username = input("Type your name: ")
-                phone_number = input("Type your phone number: ")
+                print("Type your name: ")
+                username = input(">>>")
+                print("Type your phone number: ")
+                phone_number = input(">>>")
                 if username and phone_number == json_object["username" and "phone_number"]:
 
                     with open('UserPass.json', 'r') as openfile:
                         json_object = json.load(openfile)
 
                     if username and phone_number == json_object["username" and "phone_number"]:
-                        change_password_new = input("Type new password: ")
+                        print("Type new password: ")
+                        change_password_new = input(">>>")
                         password = change_password_new
                         json_object["password"] = change_password_new
 
@@ -96,7 +154,7 @@ elif register == "Y":
                         json_object = json.dumps(information, indent=3)
                         outfile.close()
 
-                else:
-                    print("Something gone wrong")
+            else:
+                print("Something gone wrong")
 else:
     print("Something gone wrong")
